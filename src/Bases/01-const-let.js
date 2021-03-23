d7b644120da25ab9c877d130109eeb25059997b3
Saya <Darktobi1616@hotmail.com>
2021-03-23 11:55:56 -0600

    Const - let - Templatestrings

---
+++ src/Bases/01-const-let.js
@@ -0,0 +1,19 @@
+
+//Variables y Constantes
+
+const nombre = "Saya";
+
+let valorDado = '2';
+
+
+console.log(nombre, valorDado);
+
+
+if (true ) {
+
+    let valorDado = 5;
+
+    console.log(valorDado)
+}
+
+console.log(valorDado)
